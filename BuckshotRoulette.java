b6843f805fa6bcd98899be730f440bf145e1d2d6
JoshYod <joshuadayne@icloud.com>
2025-05-16 19:42:42 +0000

    update BuckshotRoulette.java and makefile

--- BuckshotRoulette.java
+++ BuckshotRoulette.java
@@ -3,10 +3,14 @@ import java.util.*;
 
 public class BuckshotRoulette{
 	public static void main(String[] args){
+		System.out.println("----------------------------------------");
+		System.out.println("Running BuckshotRoulette...");
 		BuckshotRoulette BR = new BuckshotRoulette();
 	}//end main
 
 	public BuckshotRoulette(){
+	
 	}//end constructor
 }//end class
+
 //end file
